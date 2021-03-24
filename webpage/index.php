96de14106e9215e93d00ab4b0dd992e7e415e75a
Abdulqodir <1910261.abdulqodir.rahimjonov@gmail.com>
2021-03-25 01:00:05 +0500

    First exercise was done !

---
+++ webpage/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Buy Your Way to a Better Education!</title>
+    <link href="buyagrade.css" type="text/css" rel="stylesheet"/>
+</head>
+
+<body>
+<form action="http://httpbin.org/post">
+    <h1>Buy Your Way to a Better Education!</h1>
+
+    <p>
+        Recent changes in University of Washington policy, now allow us to offer grades for money. That's right! All you
+        need to get a A in this course is cold, hard, cash.
+    </p>
+
+    <hr/>
+
+    <h2>Give Us Your Money</h2>
+    <dl>
+        <dt>Name</dt>
+        <dd>
+            <input type="text" name="username">
+        </dd>
+
+        <dt>Section</dt>
+        <dd>
+            <select name="section">
+                <option selected disabled>(Select a section)</option>
+                <option value="ma">MA</option>
+                <option value="mb">MB</option>
+                <option value="mc">MC</option>
+                <option value="md">MD</option>
+                <option value="mf">MF</option>
+                <option value="mg">MG</option>
+                <option value="mh">MH</option>
+            </select>
+        </dd>
+
+        <dt>Credit Card</dt>
+        <dd>
+            <input type="text" name="CreditCard">
+            <br>
+            <input type="radio" name="CreditCard" value="Visa">Visa
+            <input type="radio" name="CreditCard" value="MasterCard">MasterCard
+        </dd>
+
+
+    </dl>
+
+    <div>
+        <input type="submit" value="I am a giant sucker."
+    </div>
+</form>
+</body>
+</html>
